c37928306821b50523b6d2e242c040162ffa419c
0x-singularity <garrettmcguire1@proton.me>
2026-02-10 19:19:35 -0500

    day 1 solution

---
+++ 2015/day1/main.py
@@ -0,0 +1,15 @@
+file = open("input.txt")
+content = file.read()
+
+current_floor, position = 0,0
+
+for char in content:
+    position+=1
+    if char == "(":
+        current_floor+=1
+    elif char == ")":
+        current_floor-=1
+    if current_floor == -1:
+        break
+
+print(position)
